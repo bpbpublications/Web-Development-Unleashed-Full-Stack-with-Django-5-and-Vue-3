30b4c552efc6d7189b6dd46dc9f99dc704d50b16
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2025-06-09 05:42:51 +0100

    Search results component

---
+++ Chapter-01/SearchResults.vue
@@ -0,0 +1,27 @@
+<script>
+import { ref } from 'vue'
+import useSearch from './composables/useSearch'
+
+export default {
+  setup() {
+    const query = ref('')
+    const { results, loading } = useSearch(query)
+    
+    return {
+      query,
+      results,
+      loading
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+  <input v-model="query" placeholder="Search..." />
+  <p v-if="loading">Loading...</p>
+  <ul v-else>
+  <li v-for="item in results" :key="item.id">{{ item.name }}</li>
+  </ul>
+</div>
+</template>
